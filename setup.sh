096472009eb4126ad938873c49cb70e83876f051
Márcio Neto <marciorvneto@gmail.com>
2022-11-28 09:54:32 -0300

    Fixed ln command and added VimPlug

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Create a simlink to .vimrc
+ln -rsf .vimrc ~/.vimrc
 
-ln -sf .vimrc ~/.vimrc
+# Download and install VimPlug
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
